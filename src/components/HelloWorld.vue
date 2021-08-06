33f4744914140a573fa08228705558f3e05a9ce5
kai01.hu <kai01.hu@wz-inc.com>
2021-08-06 15:06:04 +0800

    已完成基本的简化数据功能

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="header">
+    <div class="buttomWarper">
+      <div id="clear" class="buttom" @click="cleardata">清除内容</div>
+      <div id="copy" class="buttom" @click="copydata">复制结果</div>
+    </div>
+  </div>
+  <div class="warper">
+    <textarea id="inputArea" v-model="inputdata" />
+    <textarea id="outputArea" v-model="outputdata" ref="outputArea" />
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref, watch } from 'vue';
+import { ElNotification } from 'element-plus';
+
+export default defineComponent({
+  components: {
+    ElNotification
+  },
+  setup() {
+    const inputdata = ref('');
+    const outputdata = ref('');
+    const outputArea = ref('');
+    watch(inputdata, () => {
+      readAndProcess();
+    });
+    const readAndProcess = () => {
+      const rawdata = inputdata.value.trim();
+      if (rawdata) {
+        const objdata = JSON.parse(rawdata);
+        const simplifieddata = simplifyData(objdata);
+        outputdata.value = JSON.stringify(simplifieddata);
+      } else {
+        outputdata.value = '';
+      }
+    };
+    const simplifyData = (data) => {
+      if (data instanceof Array) {
+        let ret = [];
+        if (data[0] instanceof Object || data.length >= 3) {
+          ret[0] = simplifyData(data[0]);
+        } else {
+          ret = data;
+        }
+        return ret;
+      } else if (data instanceof Object) {
+        let ret = {};
+        const keys = Object.keys(data);
+        keys.forEach((key) => {
+          ret[key] = simplifyData(data[key]);
+        });
+        return ret;
+      } else {
+        return data;
+      }
+    };
+    const cleardata = (e) => {
+      e.target.style.backgroundColor = '#ffe600';
+      setTimeout(() => {
+        e.target.style.backgroundColor = '#409eff';
+      }, 80);
+      inputdata.value = '';
+    };
+    const copydata = (e) => {
+      e.target.style.backgroundColor = '#ffe600';
+      setTimeout(() => {
+        e.target.style.backgroundColor = '#409eff';
+      }, 80);
+      outputArea.value.select();
+      try {
+        document.execCommand('copy');
+        ElNotification({
+          title: '成功',
+          message: '复制成功！',
+          type: 'success',
+          duration: 800,
+          position: 'top-right'
+        });
+      } catch (e) {
+        ElNotification({
+          title: '失败',
+          message: '复制失败！',
+          type: 'error',
+          duration: 800,
+          position: 'top-right'
+        });
+      }
+    };
+    return {
+      inputdata,
+      outputdata,
+      outputArea,
+      readAndProcess,
+      simplifyData,
+      cleardata,
+      copydata
+    };
+  }
+});
+</script>
+
+
+<style scoped lang="scss">
+.header {
+  height: 60px;
+  padding-bottom: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: dimgray;
+  .buttomWarper {
+    display: flex;
+    justify-content: space-around;
+    .buttom {
+      background-color: #409eff;
+      border: #25619d 1px solid;
+      border-radius: 7px;
+      color: white;
+      width: 300px;
+      height: 45px;
+      margin-top: 15px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 24px;
+      user-select: none;
+      &:hover {
+        background-color: #60aeff;
+      }
+    }
+    #clear {
+      margin-right: 10px;
+    }
+    #copy {
+      margin-left: 10px;
+    }
+  }
+}
+.warper {
+  display: flex;
+  padding: 0 40px 20px 40px;
+  background-color: dimgray;
+  height: calc(100% - 90px);
+  #inputArea {
+    width: 50%;
+    height: calc(100% - 26px);
+    background-color: bisque;
+    display: inline-block;
+    margin-right: 10px;
+    border-radius: 5px;
+    resize: none;
+  }
+  #outputArea {
+    width: 50%;
+    height: calc(100% - 26px);
+    background-color: floralwhite;
+    display: inline-block;
+    margin-left: 10px;
+    border-radius: 5px;
+    resize: none;
+  }
+}
+</style>
