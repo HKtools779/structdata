cc18e36c7b6693ef269d703ea09aa44d9bb63fda
hk <2971913255@qq.com>
2021-08-08 23:02:40 +0800

    更该狂赌

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,17 +12,17 @@
 </template>
 
 <script>
-import { defineComponent, ref, watch } from 'vue';
-import { ElNotification } from 'element-plus';
+import { defineComponent, ref, watch } from "vue";
+import { ElNotification } from "element-plus";
 
 export default defineComponent({
   components: {
-    ElNotification
+    ElNotification,
   },
   setup() {
-    const inputdata = ref('');
-    const outputdata = ref('');
-    const outputArea = ref('');
+    const inputdata = ref("");
+    const outputdata = ref("");
+    const outputArea = ref("");
     watch(inputdata, () => {
       readAndProcess();
     });
@@ -33,7 +33,7 @@ export default defineComponent({
         const simplifieddata = simplifyData(objdata);
         outputdata.value = deepstruct(simplifieddata, 0);
       } else {
-        outputdata.value = '';
+        outputdata.value = "";
       }
     };
     const simplifyData = (data) => {
@@ -57,42 +57,42 @@ export default defineComponent({
       }
     };
     const depthspace = (depth) => {
-      let ret = '';
+      let ret = "";
       for (let i = 0; i < depth; i++) {
-        ret += '    ';
+        ret += "    ";
       }
       return ret;
     };
     const deepstruct = (data, depth) => {
-      let ret = '';
+      let ret = "";
       if (data instanceof Array) {
-        ret += '[\n';
+        ret += "[\n";
         for (let i = 0; i < data.length; i++) {
           ret += depthspace(depth + 1);
           ret += deepstruct(data[i], depth + 1);
           if (i != data.length - 1) {
-            ret += ',';
+            ret += ",";
           }
-          ret += '\n';
+          ret += "\n";
         }
         ret += depthspace(depth);
-        ret += ']';
+        ret += "]";
       } else if (data instanceof Object) {
-        ret += '{\n';
+        ret += "{\n";
         const keys = Object.keys(data);
         for (let i = 0; i < keys.length; i++) {
           ret += depthspace(depth + 1);
           ret += `\"${keys[i]}\":`;
           ret += deepstruct(data[keys[i]], depth + 1);
           if (i != keys.length - 1) {
-            ret += ',';
+            ret += ",";
           }
-          ret += '\n';
+          ret += "\n";
         }
         ret += depthspace(depth);
-        ret += '}';
-      } else if (typeof data == 'string') {
-        console.log('String,', data);
+        ret += "}";
+      } else if (typeof data == "string") {
+        console.log("String,", data);
         ret += `\"${data}\"`;
       } else {
         ret += data;
@@ -101,34 +101,34 @@ export default defineComponent({
     };
 
     const cleardata = (e) => {
-      e.target.style.backgroundColor = '#ffe600';
+      e.target.style.backgroundColor = "#ffe600";
       setTimeout(() => {
-        e.target.style.backgroundColor = '#409eff';
+        e.target.style.backgroundColor = "#409eff";
       }, 80);
-      inputdata.value = '';
+      inputdata.value = "";
     };
     const copydata = (e) => {
-      e.target.style.backgroundColor = '#ffe600';
+      e.target.style.backgroundColor = "#ffe600";
       setTimeout(() => {
-        e.target.style.backgroundColor = '#409eff';
+        e.target.style.backgroundColor = "#409eff";
       }, 80);
       outputArea.value.select();
       try {
-        document.execCommand('copy');
+        document.execCommand("copy");
         ElNotification({
-          title: '成功',
-          message: '复制成功！',
-          type: 'success',
+          title: "成功",
+          message: "复制成功！",
+          type: "success",
           duration: 800,
-          position: 'top-right'
+          position: "top-right",
         });
       } catch (e) {
         ElNotification({
-          title: '失败',
-          message: '复制失败！',
-          type: 'error',
+          title: "失败",
+          message: "复制失败！",
+          type: "error",
           duration: 800,
-          position: 'top-right'
+          position: "top-right",
         });
       }
     };
@@ -141,29 +141,32 @@ export default defineComponent({
       cleardata,
       copydata,
       depthspace,
-      deepstruct
+      deepstruct,
     };
-  }
+  },
 });
 </script>
 
 <style scoped lang="scss">
 .header {
   height: 60px;
-  padding-bottom: 10px;
+  padding: 0px 40px 10px;
+  min-width: 400px;
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: dimgray;
   .buttomWarper {
     display: flex;
-    justify-content: space-around;
+    justify-content: center;
+    width: 100%;
     .buttom {
       background-color: #409eff;
       border: #25619d 1px solid;
       border-radius: 7px;
       color: white;
-      width: 300px;
+      width: 50%;
+      min-width: 150px;
       height: 45px;
       margin-top: 15px;
       display: flex;
@@ -188,6 +191,7 @@ export default defineComponent({
   padding: 0 40px 20px 40px;
   background-color: dimgray;
   height: calc(100% - 90px);
+  min-width: 400px;
   #inputArea {
     width: 50%;
     height: calc(100% - 26px);
